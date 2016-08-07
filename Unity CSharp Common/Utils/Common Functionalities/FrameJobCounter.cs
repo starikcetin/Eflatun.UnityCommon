d7859cd77eee5cb4b2c5df1403255fe364805478
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2016-08-07 07:52:56 +0300

    Created a new class named FrameJobCounter. Belongs to Common Functionalities.
    This class helps executing frame-divided (or frame-limited) coroutine jobs.

---
+++ Unity CSharp Common/Utils/Common Functionalities/FrameJobCounter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace UnityCSharpCommon.Utils.Common
+{
+    /// <summary>
+    /// A job counter for jobs that has limits per frame.
+    /// </summary>
+    public class FrameJobCounter
+    {
+        private int _lastFrame;
+
+        public int JobLimit { get; private set; }
+        public int JobCount { get; private set; }
+
+        /// <summary>
+        /// Indicates if job limit is 'not' reached for this frame.
+        /// </summary>
+        public bool CanWork
+        {
+            get
+            {
+                // Get the current frame number.
+                int currentFrame = Time.frameCount;
+
+                // If the last frame number and current frame number are different (which means at least one frame has been passed)...
+                if (_lastFrame != currentFrame)
+                {
+                    // Reset the job counter.
+                    JobCount = 0;
+
+                    // Save the frame number.
+                    _lastFrame = currentFrame;
+                }
+
+                // Return if job limit is *not* reached.
+                return JobCount < JobLimit;
+            }
+        }
+
+        public FrameJobCounter(int jobLimit)
+        {
+            JobLimit = jobLimit;
+            JobCount = 0;
+        }
+
+        /// <summary>
+        /// Call when a job is finished.
+        /// </summary>
+        public void JobDone()
+        {
+            JobCount++;
+        }
+    }
+}
