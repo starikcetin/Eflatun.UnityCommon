899939f8b5cfa8ce18bb4b8b9fda771a2a950959
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2016-08-19 15:20:25 +0300

    Added basic UI utilities.

---
+++ Unity CSharp Common/Utils/UI/ApplicationQuitButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityCSCommon.Utils.UI
+{
+    /// <summary>
+    /// A Button that quits the application when clicked.
+    /// </summary>
+    public class ApplicationQuitButton : ButtonBase
+    {
+        protected override void OnClick()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Unity CSharp Common/Utils/UI/Panel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityCSCommon.Utils.UI
+{
+    /// <summary>
+    /// A component to identify and interfere with Panel gameobjects.
+    /// </summary>
+    public class Panel : MonoBehaviour
+    {
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
