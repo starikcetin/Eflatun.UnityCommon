7f971afa20c106afb9f131fa59969b3effbc2655
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2016-08-30 12:31:40 +0300

    implemented EnumUtilities

---
+++ Unity CSharp Common/Utils/Common Functionalities/EnumUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityCSCommon.Utils.Common
+{
+    /// <summary>
+    /// Utilities for <see cref="Enum"/>.
+    /// </summary>
+    public static class EnumUtilities
+    {
+        /// <summary>
+        /// Returns an <see cref="IEnumerator{T}"/> containing all values of <typeparamref name="T"/>.
+        /// </summary>
+        public static IEnumerable<T> GetValues<T>() where T : struct, IComparable, IConvertible, IFormattable
+        {
+            return (T[]) Enum.GetValues(typeof(T));
+        }
+    }
+}
