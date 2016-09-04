23cdea9a7efff17618e7dfd29e5d4c5f54f0c2f6
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2016-09-04 23:33:08 +0300

    implemented ToggleGraphicChanger

---
+++ Unity CSharp Common/Utils/UI/ToggleGraphicChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityCSCommon.Utils.UI
+{
+    /// <summary>
+    /// Changes the graphic of <see cref="Toggle"/> depending on it's state.
+    /// </summary>
+    [RequireComponent(typeof(Toggle))]
+    public class ToggleGraphicChanger : MonoBehaviour
+    {
+        [SerializeField] private Graphic _onGraphic;
+        [SerializeField] private Graphic _offGraphic;
+
+        void Start()
+        {
+            GetComponent<Toggle>().onValueChanged.AddListener (ChangeGraphic);
+        }
+
+        private void ChangeGraphic (bool value)
+        {
+            _onGraphic.gameObject.SetActive (value);
+            _offGraphic.gameObject.SetActive (!value);
+        }
+    }
+}
