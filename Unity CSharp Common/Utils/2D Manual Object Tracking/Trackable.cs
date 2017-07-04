5584b34467078c6fe9b868c0fd34d03ca9d228ba
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2017-07-04 18:14:00 +0300

    Removed 2D Manual Object Tracking namespace with everything in it.
    ## Reason
    This was a failed attempt to replace Unity's built-in physics engine wtih only features I wanted, while developing SpaceNations.
    It kind of did what I wanted it to: it was predictable, constant, and stable. But performance-wise it wasn't good enough -- it was still better than Unity's Box2D though.
    Due to the fact that Unity guys are now exposing Box2D internals, this library is beyond redundant.
    That being said, after that disappointment with SpaceNations project, Unity will not be my first choice for a performance-critical 2D game anymore.

--- Unity CSharp Common/Utils/2D Manual Object Tracking/Trackable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace UnityCSCommon.Utils.ManualTracking2D
-{
-    /// <summary>
-    /// This is a dummy component to declare that this gameObject is trackable by ObjectDatabase.
-    /// </summary>
-    public class Trackable : MonoBehaviour { }
-}
